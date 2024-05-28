47cc7cd1609ad9980d6a1781c85d1abdf0e3d9ad
Simonaognyanova05 <simonaognanova05@gmail.com>
2024-05-28 21:54:29 +0300

    Add restaurant get request

--- src/user/restaurant.js
+++ src/user/restaurant.js
@@ -1,3 +1,6 @@
-module.exports = (req, res) => {
-    res.render('user/restaurant', {title: "Ресторант"})
+const { getRestaurant } = require('../../requests/user/getRestaurant')
+
+module.exports = async (req, res) => {
+    const restaurant = await getRestaurant(req, res);
+    res.render('user/restaurant', { title: "Ресторант", restaurant })
 }
